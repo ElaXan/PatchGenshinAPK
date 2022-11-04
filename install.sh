2361b8e80f647db3613007fe8e91d5dbf5c998e0
ElaXan <zero@elaxan.com>
2022-11-05 06:56:19 +0700

    Fix install

--- install.sh
+++ install.sh
@@ -9,14 +9,11 @@ pathPatch="$path/PatchGenshinAPK"
 if [ -d $pathPatch ]; then
     rm -rf $pathPatch
 fi
-if [ -f "$PREFIX/bin/patchgenshin" ]; then
-    rm "$PREFIX/bin/patchgenshin"
-fi
 cd $path
 git clone https://github.com/Score-Inc/PatchGenshinAPK
 cd $pathPatch
 pip3 install -r requirements.txt
-ln -sv $pathPatch/main.py $PREFIX/bin/patchgenshin
+ln -svf $pathPatch/main.py $PREFIX/bin/patchgenshin
 chmod +x $PREFIX/bin/patchgenshin
 if [[ -f "$PREFIX/bin/patchgenshin" ]]; then
     echo "Install Success, now enter command \"patchgenshin\""
